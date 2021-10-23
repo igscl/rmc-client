2613a0eb2e4f955bdbc90426472276703827244a
Ignacio Giadach <igiadach@gmail.com>
2021-10-23 17:18:32 +1100

    fixed multiple requests for unmonted component in useEffect

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -10,20 +10,30 @@ const Confirmation = ({joinNodeId}) => {
 
     const [name, setName] = useState("")
     useEffect(() => {
+        let isMounted = true
         if(name === ""){
             loggedInUser &&
             joinNode(joinNodeId).then((response) => {
+                if (isMounted){
                 console.log("join node",response)
-                setName(response.name)
+                setName(response.name)}
             })
+            return () => {isMounted = false}
         }
             console.log("running again!")
-    }, [loggedInUser])
+    }, [loggedInUser, joinNodeId, name])
 
     return(
         <div>
             {loggedInUser ? (
-                <h2>Joined {} node</h2>
+                <div className="bg-white-50">
+                    <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+                        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                            <span className="block">Joined {name}</span>
+                        </h2>
+                    </div>
+                </div>
+
             ):(
                 <Login redirectPath={`/node/join/${joinNodeId}`}/>
             )}
